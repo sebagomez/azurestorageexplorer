58ab5e72c3cbd1c96c78220fac3e653d93e27b85
sebagomez <sebagomezcorrea@outlook.com>
2016-01-07 12:56:25 -0300

    fixed 404 redirects

--- StorageManager/404.aspx.cs
+++ StorageManager/404.aspx.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using StorageManager.Resources;
 
 namespace StorageManager
 {
-    public partial class _04 : System.Web.UI.Page
+	public partial class _04 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
