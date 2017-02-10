314ad6fcd2cd4ba56470fc27662953af4978bb66
sebagomez <sebagomezcorrea@outlook.com>
2017-02-10 10:34:31 -0300

    UniTest project added

---
+++ tests/APIUnitTests/BaseTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIUnitTests
+{
+	[TestClass]
+	public class BaseTests
+	{
+		public readonly string ACCOUNT = Settings.Instance.Account;
+		public readonly string KEY = Settings.Instance.Key;
+
+		internal string GetStringGuid()
+		{
+			return Guid.NewGuid().ToString().ToLower().Replace("-", "");
+		}
+	}
+}
