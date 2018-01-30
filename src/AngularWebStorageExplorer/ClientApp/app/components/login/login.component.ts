925a154325bedceb1a8857dfc72a53ebc895d18c
sebastian gomez <sebagomezcorrea@outlook.com>
2018-01-30 10:51:41 -0300

    improved login experience

--- src/AngularWebStorageExplorer/ClientApp/app/components/login/login.component.ts
+++ src/AngularWebStorageExplorer/ClientApp/app/components/login/login.component.ts
@@ -16,12 +16,18 @@ export class LoginComponent {
 	//https://yakovfain.com/2016/10/31/angular-2-component-communication-with-events-vs-callbacks/
 	@Output() signedIn: EventEmitter<boolean> = new EventEmitter();
 
+	public loading: boolean = false;
+	public showError: boolean = false;
+
 	constructor(utils: UtilsService) {
 
 		this.utilsService = utils;
 	}
 
 	signIn() {
+		this.loading = true;
+		this.showError = false;
+
 		let account = encodeURIComponent(this.azureAccount.nativeElement.value);
 		let key = encodeURIComponent(this.azureKey.nativeElement.value);
 
@@ -29,19 +35,25 @@ export class LoginComponent {
 			localStorage.setItem('account', account);
 			localStorage.setItem('key', key);
 
+			this.loading = false;
 			this.signedIn.emit(true);
 
 		}, error => {
 
 			localStorage.clear();
 
+			this.loading = false;
 			this.signedIn.emit(false);
+
+			this.showError = true;
 
 			console.error(error)
 		});
 	}
 
 	typingMessage(event: KeyboardEvent) {
+		if (this.showError)
+			this.showError = false;
 		if (event.key == 'Enter')
 			this.signIn();
 	}
