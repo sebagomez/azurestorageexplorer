4acfc83afd39b009b3b2a7dbceaa251324c6a4c3
Sebastian Gomez <sebastiangomezcorrea@gmail.com>
2021-05-21 16:46:16 -0400

    npm update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,2 @@
-cd src/AzureWebStorageExplorer/ClientApp/
-npm update
-cd ../../../
 rm -rf ./bin/**
 dotnet publish --configuration Release ./src/AzureWebStorageExplorer/AzureWebStorageExplorer.csproj -o ./bin
