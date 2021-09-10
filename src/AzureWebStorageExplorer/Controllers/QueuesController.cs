0863a262865b71758d3dad94cfa7273fb746868c
Sebastian Gomez <sebastiangomezcorrea@gmail.com>
2021-09-10 11:00:25 -0400

    emoved old WindowsAzure ref for Queues

--- src/AzureWebStorageExplorer/Controllers/QueuesController.cs
+++ src/AzureWebStorageExplorer/Controllers/QueuesController.cs
@@ -1,74 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.WindowsAzure.Storage.Queue;
+
 using StorageLibrary;
+using StorageLibrary.Common;
 
 namespace AzureWebStorageExplorer.Controllers
 {
-	[Produces("application/json")]
-	[Route("api/Queues")]
-	public class QueuesController : Controller
-	{
-		// GET: api/Queues
-		[HttpGet("[action]")]
-		public async Task<IEnumerable<string>> GetQueues(string account, string key)
-		{
-			return await Queue.ListQueuesAsync(account, key);
-		}
+    [Produces("application/json")]
+    [Route("api/Queues")]
+    public class QueuesController : Controller
+    {
+        // GET: api/Queues
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<string>> GetQueues(string account, string key)
+        {
+            return await Queue.ListQueuesAsync(account, key);
+        }
 
-		[HttpGet("[action]")]
-		public async Task<IEnumerable<KeyValuePair<string,string>>> GetMessages(string account, string key, string queue)
-		{
-			List<CloudQueueMessage> messages = await Queue.GetAllMessagesAsync(account, key, queue);
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<KeyValuePair<string, string>>> GetMessages(string account, string key, string queue)
+        {
+            List<PeekedMessageWrapper> messages = await Queue.GetAllMessagesAsync(account, key, queue);
 
-			return messages.Select(m => new KeyValuePair<string, string>(m.Id, m.AsString));
-		}
+            return messages.Select(m => new KeyValuePair<string, string>(m.Id, m.Message));
+        }
 
-		[HttpPost("[action]")]
-		public async Task<IActionResult> DeleteMessage(string account, string key, string queue, string messageId)
-		{
-			if (string.IsNullOrEmpty(queue) || string.IsNullOrEmpty(messageId))
-				return BadRequest();
+        [HttpPost("[action]")]
+        public async Task<IActionResult> DequeueMessage(string account, string key, string queue)
+        {
+            if (string.IsNullOrEmpty(queue))
+                return BadRequest();
 
-			await Queue.DeleteMessage(account, key, queue, messageId);
+            await Queue.DequeueMessage(account, key, queue);
 
-			return Ok();
-		}
+            return Ok();
+        }
 
-		[HttpPost("[action]")]
-		public async Task<IActionResult> NewQueue(string account, string key, string queue)
-		{
-			if (string.IsNullOrEmpty(queue))
-				return BadRequest();
+        [HttpPost("[action]")]
+        public async Task<IActionResult> NewQueue(string account, string key, string queue)
+        {
+            if (string.IsNullOrEmpty(queue))
+                return BadRequest();
 
-			await Queue.CreateAsync(account, key, queue);
+            await Queue.CreateAsync(account, key, queue);
 
-			return Ok();
-		}
+            return Ok();
+        }
 
-		[HttpPost("[action]")]
-		public async Task<IActionResult> NewQueueMessage(string account, string key, string queue, string message)
-		{
-			if (string.IsNullOrEmpty(queue))
-				return BadRequest();
+        [HttpPost("[action]")]
+        public async Task<IActionResult> NewQueueMessage(string account, string key, string queue, string message)
+        {
+            if (string.IsNullOrEmpty(queue))
+                return BadRequest();
 
-			await Queue.CreateMessageAsync(account, key, queue, message);
+            await Queue.CreateMessageAsync(account, key, queue, message);
 
-			return Ok();
-		}
+            return Ok();
+        }
 
-		[HttpPost("[action]")]
-		public async Task<IActionResult> DeleteQueue(string account, string key, string queue)
-		{
-			if (string.IsNullOrEmpty(queue))
-				return BadRequest();
+        [HttpPost("[action]")]
+        public async Task<IActionResult> DeleteQueue(string account, string key, string queue)
+        {
+            if (string.IsNullOrEmpty(queue))
+                return BadRequest();
 
-			await Queue.DeleteAsync(account, key, queue);
+            await Queue.DeleteAsync(account, key, queue);
 
-			return Ok();
-		}
-	}
+            return Ok();
+        }
+    }
 }
---
+++ src/StorageLibrary/Common/PeekedMessageWrapper.cs
@@ -0,0 +1,8 @@
+﻿namespace StorageLibrary.Common
+{
+    public class PeekedMessageWrapper
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
+    }
+}
