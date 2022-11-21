9055cb8b8f4d17d822828677054a372c18529b7e
Sebastián Gómez <sebastiangomezcorrea@gmail.com>
2022-11-20 20:05:21 -0500

    Added favicon and new screenshot

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 cd "${0%/*}" || exit 1
 
-ehoo App will run on http://localhost:8080
+echo App will run on http://localhost:8080
 
-docker run --rm -p 8080:8080 --name azurestorageexplorer azurestorageexplorer:local
+docker run --rm -p 8080:80 --name azurestorageexplorer azurestorageexplorer:local
